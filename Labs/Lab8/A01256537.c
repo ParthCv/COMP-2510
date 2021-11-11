1b5520db99280c901b9f46ff2cf711538e436b89
Parth Chaturvedi <pchaturvedi@my.bcit.ca>
2021-11-10 21:53:05 -0800

    Fix Lab 10

--- Labs/Lab8/A01256537.c
+++ Labs/Lab8/A01256537.c
@@ -5,6 +5,7 @@
 #define ASCII_GAP 32
 #define EXPECTED_NUMBER_OF_ARGUMENTS 2
 #define INVALID_NUMBER_OF_ARGUMENTS_ERROR 1
+#define MEMORY_ALLOCATION_ERROR 2
 
 
 void correctNumberOfArguments(int argc) {
@@ -14,29 +15,29 @@ void correctNumberOfArguments(int argc) {
     }
 }
 
-void transformString(char *input, char *output) {
+void convertString(char *input, char *output) {
     if (!*input) {
         *output = '\0';
         return;
     }
     if (*input == ' ') {
-        transformString(input + 1, output);
+        convertString(input + 1, output);
     } else if (*input >= 'a' && *input <= 'z') {
         int curChar = (int) *input;
         *output = (char) (curChar - ASCII_GAP);
-        transformString(input + 1, output + 1);
+        convertString(input + 1, output + 1);
     } else if (*input == '*') {
         *output = *input;
         output++;
         *output = *input;
-        transformString(input + 1, output + 1);
+        convertString(input + 1, output + 1);
     } else {
         *output = *input;
-        transformString(input + 1, output + 1);
+        convertString(input + 1, output + 1);
     }
 }
 
-size_t allocateOutputSize(char *input) {
+size_t getOutputSize(char *input) {
     size_t expectedSize = strlen(input) + 1;
     while (*input) {
         if (*input == ' ')
@@ -48,19 +49,24 @@ size_t allocateOutputSize(char *input) {
     return expectedSize;
 }
 
+void checkMemoryAllocation(const char *string){
+    if(string == NULL){
+        perror("Memory Allocation Error");
+        exit(MEMORY_ALLOCATION_ERROR);
+    }
+}
+
 void processString(char *input) {
-    size_t size = allocateOutputSize(input);
+    size_t size = getOutputSize(input);
     char *output = (char *) malloc(size * sizeof(char));
-    transformString(input, output);
+    checkMemoryAllocation(output);
+    convertString(input, output);
     printf("\"%s\" -> \"%s\"",input,output);
     free(output);
 }
 
 int main(int argc, char *argv[]) {
-    //correctNumberOfArguments(argc);
-    printf("%s\n",argv[0]);
-    printf("%s\n",argv[2]);
-
+    correctNumberOfArguments(argc);
     processString(argv[1]);
     return 0;
 }
