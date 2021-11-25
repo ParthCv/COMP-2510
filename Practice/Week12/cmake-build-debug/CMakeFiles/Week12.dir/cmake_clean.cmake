8e1efbb042bc0d2e398c5d670b2ce273afe13692
Parth Chaturvedi <pchaturvedi@my.bcit.ca>
2021-11-24 18:02:32 -0800

    add gitignore

---
+++ Practice/Week12/cmake-build-debug/CMakeFiles/Week12.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Week12.dir/main.c.obj"
+  "Week12.exe"
+  "Week12.exe.manifest"
+  "Week12.pdb"
+  "libWeek12.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Week12.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Practice/Week12/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+struct Node{
+    float data;
+    struct Node* next;
+};
+
+typedef struct Node* Link;
+
+
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
